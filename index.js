99c3112db0f634d1b5954b5ffc4ba383f49c8ff1
Joseph Etim Ibok <josephibok36@gmail.com>
2023-12-22 19:05:00 +0100

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.post("/burn-list", (req, res) => {
+    console.log(req.body);
+});
+
+app.get("/burn-list", (req, res) => {
+    res.json([
+        {name: "Transaction 1"},
+        {name: "Transaction 2"},
+        {name: "Transaction 3"},
+        {name: "Transaction 4"}
+    ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+});
